6bea44428092bf8e8566959e6e9d841da2528040
chewbacca1991 <mounafbanbo@hotmail.com>
2025-10-31 12:37:03 +0330

    Update MongoDB connection URI to use a more secure format

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/api/spendings', spendingRoutes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/phan-tich-du-lieu-chinh-phu', { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/phan-tich-du-lieu-chinh-phu';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
     .catch(err => console.error(err));
