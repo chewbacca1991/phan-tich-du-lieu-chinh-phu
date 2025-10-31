5a370dda11e324da695e9edbf615880909af1bf9
chewbacca1991 <mounafbanbo@hotmail.com>
2025-10-31 12:33:56 +0330

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const spendingRoutes = require('./routes/spending');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api/spendings', spendingRoutes);
+
+mongoose.connect('mongodb://localhost:27017/phan-tich-du-lieu-chinh-phu', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
+    .catch(err => console.error(err));
