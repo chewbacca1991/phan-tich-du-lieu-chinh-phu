b562917b3a547b2acb60780d92547485ce0d0171
chewbacca1991 <mounafbanbo@hotmail.com>
2025-10-31 12:34:52 +0330

    Update MongoDB connection URL to use environment variable for better security.

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/api/spendings', spendingRoutes);
 
-mongoose.connect('mongodb://localhost:27017/phan-tich-du-lieu-chinh-phu', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/phan-tich-du-lieu-chinh-phu', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
     .catch(err => console.error(err));
