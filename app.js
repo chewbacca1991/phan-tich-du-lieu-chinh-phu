3d3ad9b5ffb4d843c623dd5bf11f6c860c9840de
chewbacca1991 <mounafbanbo@hotmail.com>
2025-10-31 12:33:49 +0330

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/phan-tich-du-lieu-chinh-phu', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Kết nối tới MongoDB thành công!');
+}).catch(err => {
+    console.error('Kết nối tới MongoDB thất bại:', err);
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Chào mừng đến với dự án Phân Tích Dữ Liệu Chính Phủ!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Máy chủ đang chạy trên http://localhost:${PORT}`);
+});
